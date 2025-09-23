eeceab4296f4fe242d50ebc392e462d19d625ee5
TerminalJack <TerminalJack@sbcglobal.net>
2025-09-22 22:04:35 -0500

    Improved the Character Map Controller custom editor.

--- Assets/Spriter2UnityDX/Runtime/CharacterMapController.cs
+++ Assets/Spriter2UnityDX/Runtime/CharacterMapController.cs
@@ -5,6 +5,7 @@ using UnityEditor.SceneManagement;
 
 #if UNITY_EDITOR
 using UnityEditor;
+using UnityEditorInternal;
 #endif
 
 namespace Spriter2UnityDX
@@ -120,7 +121,7 @@ namespace Spriter2UnityDX
             return false; // Wasn't in activeMaps.
         }
 
-        public void Refresh()
+        public void Refresh(bool logWarnings = true)
         {
             ApplyMap(baseMap);
 
@@ -132,7 +133,7 @@ namespace Spriter2UnityDX
                 {
                     ApplyMap(map);
                 }
-                else
+                else if (logWarnings)
                 {
                     Debug.LogWarning($"CharacterMapController.Refresh(): The map name '{mapName}' is not valid.");
                 }
@@ -173,11 +174,11 @@ namespace Spriter2UnityDX
         [CustomEditor(typeof(CharacterMapController))]
         protected class CharacterMapController_Editor : Editor
         {
-
             private static readonly GUIContent _activeMapNamesContent = new GUIContent(
                 text: "Active Maps",
                 tooltip: "The active character maps.  Add, remove, and rearrange the names from Available Maps to " +
-                "this list and hit the 'Apply Active Maps' button to apply your changes.");
+                "this list.  Updates should be automatic but the 'Apply Active Maps' button can be used to ensure " +
+                "that all changes take effect.  (As well as validate all of the names in the list.)");
 
             private static readonly GUIContent _baseMapContent = new GUIContent(
                 text: "Base Map",
@@ -185,22 +186,117 @@ namespace Spriter2UnityDX
 
             private static readonly GUIContent _availableMapsContent = new GUIContent(
                 text: "Available Maps",
-                tooltip: "These are the character maps that are available for this prefab.  " +
-                "Add their name(s) to Active Maps to activate them.");
+                tooltip: "These are the character maps that are available for this prefab.  Add/remove their name(s) " +
+                "to Active Maps via the +/- buttons.");
 
             private static readonly GUIContent _applyActiveMapsButtonContent = new GUIContent(
                 text: "Apply Active Maps",
-                tooltip: "Click this to apply any changes that you make to Active Maps.");
+                tooltip: "Click this to apply any changes that you make to Active Maps and validate the names in the " +
+                "list.  Any invalid names will be logged to the console.");
 
             private SerializedProperty _activeMapNamesProperty;
             private SerializedProperty _baseMapProperty;
             private SerializedProperty _availableMapsProperty;
+
+            private ReorderableList _availableMapsList;
+            private bool _showAvailableMaps;
 
             private void OnEnable()
             {
                 _activeMapNamesProperty = serializedObject.FindProperty("activeMapNames");
                 _baseMapProperty = serializedObject.FindProperty("baseMap");
                 _availableMapsProperty = serializedObject.FindProperty("availableMaps");
+
+                _availableMapsList = new ReorderableList(
+                    serializedObject,
+                    _availableMapsProperty,
+                    draggable: true,
+                    displayHeader: false,
+                    displayAddButton: true,
+                    displayRemoveButton: true
+                );
+
+                _availableMapsList.elementHeightCallback = index =>
+                {
+                    var element = _availableMapsProperty.GetArrayElementAtIndex(index);
+
+                    // Base line + spacing
+                    float height = EditorGUIUtility.singleLineHeight + 4;
+
+                    // If its foldout is open, add the full height of all child properties
+                    if (element.isExpanded)
+                    {
+                        height = EditorGUI.GetPropertyHeight(element, true) + 4;
+                    }
+
+                    return height;
+                };
+
+                _availableMapsList.drawElementCallback = (rect, index, isActive, isFocused) =>
+                {
+                    rect.y += 1;
+
+                    var element = _availableMapsProperty.GetArrayElementAtIndex(index);
+
+                    // Foldout field
+                    var fieldRect = new Rect(rect.x + 10f, rect.y, rect.width - 34f, EditorGUIUtility.singleLineHeight);
+                    EditorGUI.PropertyField(fieldRect, element);
+
+                    // +/- Button on the right
+                    var addRemoveButtonRect = new Rect(
+                        fieldRect.x + fieldRect.width + 4f,
+                        rect.y + 1f,
+                        24f,
+                        EditorGUIUtility.singleLineHeight - 1f
+                    );
+
+                    var controller = (CharacterMapController)target;
+                    var mapName = controller.availableMaps[index].name;
+                    bool isActiveMapName = controller.activeMapNames.Contains(mapName);
+
+                    Color oldBG = GUI.backgroundColor;
+
+                    if (EditorGUIUtility.isProSkin)
+                    {
+                        GUI.backgroundColor = isActiveMapName
+                            ? new Color(0.75f, 0f, 0f)     // red
+                            : new Color(0.2f, 0.8f, 0.2f); // green
+                    }
+                    else
+                    {
+                        GUI.backgroundColor = isActiveMapName
+                            ? new Color(1f, 0.75f, 0.75f) // red
+                            : new Color(0.7f, 1f, 0.7f);  // green
+                    }
+
+                    if (GUI.Button(addRemoveButtonRect, isActiveMapName ? "-" : "+"))
+                    {
+                        if (isActiveMapName)
+                        {
+                            controller.Remove(mapName);
+                        }
+                        else
+                        {
+                            controller.Add(mapName);
+                        }
+
+                        EditorUtility.SetDirty(controller);
+                    }
+
+                    GUI.backgroundColor = oldBG;
+
+                    // If expanded, draw all of the children
+                    if (element.isExpanded)
+                    {
+                        var childRect = new Rect(
+                            fieldRect.x,
+                            fieldRect.y,
+                            fieldRect.width + addRemoveButtonRect.width + 2f,
+                            EditorGUI.GetPropertyHeight(element, true) - EditorGUIUtility.singleLineHeight);
+
+                        EditorGUI.PropertyField(childRect, element, GUIContent.none, includeChildren: true);
+                    }
+                };
             }
 
             public override void OnInspectorGUI()
@@ -215,35 +311,59 @@ namespace Spriter2UnityDX
                 EditorGUILayout.Space();
                 GUI.enabled = true;
 
+                EditorGUI.BeginChangeCheck();
+
                 EditorGUILayout.PropertyField(_activeMapNamesProperty, _activeMapNamesContent);
+
+                if (EditorGUI.EndChangeCheck())
+                {   // Something changed: add, remove, reorder, or edit strings
+                    EditorApplication.delayCall += () =>  RefreshCharacterMapController(characterMapController, logWarnings: false);
+                }
 
                 if (GUILayout.Button(_applyActiveMapsButtonContent))
                 {
-                    characterMapController.Refresh();
+                    RefreshCharacterMapController(characterMapController);
+                }
 
-                    foreach (var textureController in characterMapController.GetComponentsInChildren<TextureController>())
-                    {
-                        EditorUtility.SetDirty(textureController);
-                    }
+                _showAvailableMaps = EditorGUILayout.Foldout(
+                    _showAvailableMaps,
+                    _availableMapsContent,
+                    toggleOnLabelClick: true);
 
-                    foreach (var spriteRenderer in characterMapController.GetComponentsInChildren<SpriteRenderer>())
-                    {
-                        EditorUtility.SetDirty(spriteRenderer);
-                    }
-
-                    var stage = PrefabStageUtility.GetCurrentPrefabStage();
-                    if (stage != null)
-                    {
-                        EditorSceneManager.MarkSceneDirty(stage.scene);
-                    }
-
-                    AssetDatabase.SaveAssets();
+                if (_showAvailableMaps)
+                {
+                    EditorGUI.indentLevel++;
+                    _availableMapsList.DoLayoutList();
+                    EditorGUI.indentLevel--;
                 }
 
                 EditorGUILayout.PropertyField(_baseMapProperty, _baseMapContent);
-                EditorGUILayout.PropertyField(_availableMapsProperty, _availableMapsContent);
 
                 serializedObject.ApplyModifiedProperties();
+            }
+
+            private void RefreshCharacterMapController(CharacterMapController characterMapController, bool logWarnings = true)
+            {
+                characterMapController.Refresh(logWarnings);
+                EditorUtility.SetDirty(characterMapController);
+
+                foreach (var textureController in characterMapController.GetComponentsInChildren<TextureController>())
+                {
+                    EditorUtility.SetDirty(textureController);
+                }
+
+                foreach (var spriteRenderer in characterMapController.GetComponentsInChildren<SpriteRenderer>())
+                {
+                    EditorUtility.SetDirty(spriteRenderer);
+                }
+
+                var stage = PrefabStageUtility.GetCurrentPrefabStage();
+                if (stage != null)
+                {
+                    EditorSceneManager.MarkSceneDirty(stage.scene);
+                }
+
+                AssetDatabase.SaveAssets();
             }
         }
 
