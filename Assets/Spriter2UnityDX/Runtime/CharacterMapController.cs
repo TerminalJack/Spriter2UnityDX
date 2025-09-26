0a144d27d2eb8cdf0d953fd2b3636e100d6074b9
TerminalJack <TerminalJack@sbcglobal.net>
2025-09-25 23:56:13 -0500

    Added a search feature for the Active Map Names field and the Available Maps field.

--- Assets/Spriter2UnityDX/Runtime/CharacterMapController.cs
+++ Assets/Spriter2UnityDX/Runtime/CharacterMapController.cs
@@ -207,10 +207,14 @@ namespace Spriter2UnityDX
             private SerializedProperty _availableMapsProperty;
 
             private bool _showAvailableMaps;
+            private string _availableMapsSearchString = "";
             private ReorderableList _availableMapsList;
+            private List<int> _availableMapsIndexMap = new List<int>();
 
             private bool _showActiveMapNames;
+            private string _activeMapsSearchString = "";
             private ReorderableList _activeMapNamesList;
+            private List<int> _activeMapsIndexMap = new List<int>();
 
             private CharacterMapController _characterMapController;
 
@@ -400,8 +404,6 @@ namespace Spriter2UnityDX
                 EditorGUILayout.Space();
                 GUI.enabled = true;
 
-                EditorGUI.BeginChangeCheck();
-
                 _showActiveMapNames = EditorGUILayout.Foldout(
                     _showActiveMapNames,
                     _activeMapNamesContent,
@@ -409,14 +411,7 @@ namespace Spriter2UnityDX
 
                 if (_showActiveMapNames)
                 {
-                    EditorGUI.indentLevel++;
-                    _activeMapNamesList.DoLayoutList();
-                    EditorGUI.indentLevel--;
-                }
-
-                if (EditorGUI.EndChangeCheck())
-                {   // Something changed: add, remove, reorder, or edit strings
-                    EditorApplication.delayCall += () => RefreshCharacterMapController(logWarnings: false);
+                    ShowActiveMapNames();
                 }
 
                 if (GUILayout.Button(_applyActiveMapsButtonContent))
@@ -433,14 +428,267 @@ namespace Spriter2UnityDX
 
                 if (_showAvailableMaps)
                 {
-                    EditorGUI.indentLevel++;
-                    _availableMapsList.DoLayoutList();
-                    EditorGUI.indentLevel--;
+                    ShowAvailableMapNames();
                 }
 
                 EditorGUILayout.PropertyField(_baseMapProperty, _baseMapContent);
 
                 serializedObject.ApplyModifiedProperties();
+            }
+
+            private void ShowActiveMapNames()
+            {
+                EditorGUI.indentLevel++;
+
+                _activeMapsSearchString = EditorGUILayout.TextField("Search", _activeMapsSearchString);
+
+                UpdateActiveMapsIndexMap();
+
+                EditorGUILayout.Space();
+
+                if (string.IsNullOrEmpty(_activeMapsSearchString))
+                {
+                    EditorGUILayout.Space(3);
+
+                    UpdateActiveMapsIndexMap();
+
+                    EditorGUI.BeginChangeCheck();
+
+                    _activeMapNamesList.DoLayoutList();
+
+                    if (EditorGUI.EndChangeCheck())
+                    {   // Something changed: add, remove, reorder, or edit strings
+                        EditorApplication.delayCall += () => RefreshCharacterMapController(logWarnings: false);
+                    }
+                }
+                else
+                {
+                    ShowFilteredActiveMapNames();
+                }
+
+                EditorGUI.indentLevel--;
+            }
+
+            private void ShowFilteredActiveMapNames()
+            {
+                float elementHeight = _activeMapNamesList.elementHeight;
+
+                foreach (int realIndex in _activeMapsIndexMap)
+                {
+                    string itemName = _activeMapNamesProperty
+                        .GetArrayElementAtIndex(realIndex)
+                        .stringValue;
+
+                    // Reserve a rect exactly the same height as a ReorderableList row
+                    Rect rowRect = EditorGUILayout.GetControlRect(
+                        false,
+                        elementHeight
+                    );
+
+                    rowRect.x += 6;
+                    rowRect.y += 6;
+                    rowRect.width -= 47;
+                    rowRect.height -= 3;
+
+                    // Draw a selectable label that fills that rect
+                    EditorGUI.SelectableLabel(
+                        rowRect,
+                        itemName,
+                        EditorStyles.textField
+                    );
+
+                    // '-' Button on the right
+                    var removeButtonRect = new Rect(
+                        rowRect.x + rowRect.width + 4f,
+                        rowRect.y,
+                        24f,
+                        rowRect.height - 1
+                    );
+
+                    Color oldBG = GUI.backgroundColor;
+
+                    GUI.backgroundColor = EditorGUIUtility.isProSkin
+                        ? new Color(0.75f, 0f, 0f)     // dark red
+                        : new Color(1f, 0.75f, 0.75f); // light red
+
+                    if (GUI.Button(removeButtonRect, "-"))
+                    {
+                        Event.current.Use();
+
+                        EditorApplication.delayCall += () =>
+                        {
+                            if (_characterMapController.Remove(itemName))
+                            {
+                                RefreshCharacterMapController();
+                            }
+                        };
+                    }
+
+                    GUI.backgroundColor = oldBG;
+                }
+
+                if (_activeMapsIndexMap.Count == 0)
+                {
+                    EditorGUILayout.HelpBox("No matching items.", MessageType.Info);
+                }
+                else
+                {
+                    GUILayout.Space(4);
+                    EditorGUILayout.HelpBox($"Showing {_activeMapsIndexMap.Count} out of {_activeMapNamesList.count} items", MessageType.Info);
+                }
+            }
+
+            private void ShowAvailableMapNames()
+            {
+                EditorGUI.indentLevel++;
+
+                EditorGUI.BeginChangeCheck();
+
+                _availableMapsSearchString = EditorGUILayout.TextField("Search", _availableMapsSearchString);
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    UpdateAvailableMapsIndexMap();
+                }
+
+                EditorGUILayout.Space();
+
+                if (string.IsNullOrEmpty(_availableMapsSearchString))
+                {
+                    EditorGUILayout.Space(4);
+
+                    UpdateAvailableMapsIndexMap();
+
+                    EditorGUI.BeginChangeCheck();
+
+                    _availableMapsList.DoLayoutList();
+
+                    if (EditorGUI.EndChangeCheck())
+                    {   // Something changed: add, remove, reorder, or edit strings
+                        EditorApplication.delayCall += () => RefreshCharacterMapController(logWarnings: false);
+                    }
+                }
+                else
+                {
+                    ShowFilteredAvailableMapNames();
+                }
+
+                EditorGUI.indentLevel--;
+            }
+
+            private void ShowFilteredAvailableMapNames()
+            {
+                float elementHeight = _availableMapsList.elementHeight;
+
+                foreach (int realIndex in _availableMapsIndexMap)
+                {
+                    var mapName = _characterMapController.availableMaps[realIndex].name;
+
+                    Rect rowRect = EditorGUILayout.GetControlRect(
+                        false,
+                        elementHeight + 1
+                    );
+
+                    rowRect.x += 14;
+                    rowRect.y += 6;
+                    rowRect.width -= 45;
+                    rowRect.height -= 3;
+
+                    EditorGUI.SelectableLabel(
+                        rowRect,
+                        mapName,
+                        EditorStyles.textField
+                    );
+
+                    // +/- Button on the right
+                    var addRemoveButtonRect = new Rect(
+                        rowRect.x + rowRect.width + 4f,
+                        rowRect.y + 1,
+                        24f,
+                        rowRect.height - 2
+                    );
+
+                    bool isActiveMapName = _characterMapController.activeMapNames.Contains(mapName);
+
+                    Color oldBG = GUI.backgroundColor;
+
+                    if (EditorGUIUtility.isProSkin)
+                    {
+                        GUI.backgroundColor = isActiveMapName
+                            ? new Color(0.75f, 0f, 0f)     // red
+                            : new Color(0.2f, 0.8f, 0.2f); // green
+                    }
+                    else
+                    {
+                        GUI.backgroundColor = isActiveMapName
+                            ? new Color(1f, 0.75f, 0.75f) // red
+                            : new Color(0.7f, 1f, 0.7f);  // green
+                    }
+
+                    if (GUI.Button(addRemoveButtonRect, isActiveMapName ? "-" : "+"))
+                    {
+                        Event.current.Use();
+                        GUI.FocusControl(null); // Make sure the search field losses focus.  (Fixes an issue.)
+
+                        EditorApplication.delayCall += () =>
+                        {
+                            if (isActiveMapName)
+                            {
+                                _characterMapController.Remove(mapName);
+                            }
+                            else
+                            {
+                                _characterMapController.Add(mapName);
+                            }
+
+                            RefreshCharacterMapController();
+                        };
+                    }
+
+                    GUI.backgroundColor = oldBG;
+                }
+
+                if (_availableMapsIndexMap.Count == 0)
+                {
+                    EditorGUILayout.HelpBox("No matching items.", MessageType.Info);
+                }
+                else
+                {
+                    GUILayout.Space(4);
+                    EditorGUILayout.HelpBox($"Showing {_availableMapsIndexMap.Count} out of {_availableMapsList.count} items", MessageType.Info);
+                }
+            }
+
+            private void UpdateActiveMapsIndexMap()
+            {
+                _activeMapsIndexMap.Clear();
+
+                for (int i = 0; i < _activeMapNamesProperty.arraySize; i++)
+                {
+                    string name = _activeMapNamesProperty.GetArrayElementAtIndex(i).stringValue;
+
+                    if (string.IsNullOrEmpty(_activeMapsSearchString) ||
+                        name.IndexOf(_activeMapsSearchString, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        _activeMapsIndexMap.Add(i);
+                    }
+                }
+            }
+
+            private void UpdateAvailableMapsIndexMap()
+            {
+                _availableMapsIndexMap.Clear();
+
+                for (int i = 0; i < _availableMapsProperty.arraySize; i++)
+                {
+                    var name = _characterMapController.availableMaps[i].name;
+
+                    if (string.IsNullOrEmpty(_availableMapsSearchString) ||
+                        name.IndexOf(_availableMapsSearchString, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        _availableMapsIndexMap.Add(i);
+                    }
+                }
             }
 
             private void RefreshCharacterMapController(bool logWarnings = true)
